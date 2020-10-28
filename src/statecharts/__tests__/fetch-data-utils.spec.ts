1b360a482be9631e2d9c47bf572638620828feb9
Marco Sangalli <sangalli.marco@gmail.com>
2020-10-28 18:27:21 +0100

    adding spinner

--- src/statecharts/__tests__/fetch-data-utils.spec.ts
+++ src/statecharts/__tests__/fetch-data-utils.spec.ts
@@ -1,5 +1,15 @@
 import { PokeData } from '../../shared/types'
-import { addItem, filterData, isInFavourites, mergeData, removeItem, reset, setFilter, setNextPage, setSelectedItem } from '../fetch-data-utils'
+import {
+  addItem,
+  filterData,
+  isInFavourites,
+  mergeData,
+  removeItem,
+  reset,
+  setFilter,
+  setNextPage,
+  setSelectedItem,
+} from '../fetch-data-utils'
 import { FetchContext } from '../fetch-types'
 
 import ampharos from './ampharos.json'
@@ -56,10 +66,12 @@ describe('helpers', () => {
     })
   })
 
-
   describe('setFilter()', () => {
     test('should set filter and reset offset', () => {
-      const {filteredList = [], filter, offset} = setFilter(testContext, 'ampharos')
+      const { filteredList = [], filter, offset } = setFilter(
+        testContext,
+        'ampharos'
+      )
 
       expect(offset).toEqual(0)
       expect(filter).toEqual('ampharos')
@@ -69,7 +81,7 @@ describe('helpers', () => {
 
   describe('reset()', () => {
     test('should reset list, filteredList and offset', () => {
-      const {offset, list, filteredList} = reset(testContext)
+      const { offset, list, filteredList } = reset(testContext)
       expect(offset).toEqual(0)
       expect(list?.length).toEqual(0)
       expect(filteredList?.length).toEqual(0)
@@ -78,37 +90,44 @@ describe('helpers', () => {
 
   describe('setNextPage()', () => {
     test('should set the offset', () => {
-      const {offset} = setNextPage(testContext)
+      const { offset } = setNextPage(testContext)
       expect(offset).toEqual(100)
     })
   })
 
   describe('addItem()', () => {
     test('should add a new item to favourites', () => {
-      const {favourites} = addItem(testContext, ampharosPokeData)
+      const { favourites } = addItem(testContext, ampharosPokeData)
       expect(favourites?.length).toEqual(1)
     })
   })
 
   describe('removeItem()', () => {
     test('should remove an item from favourites', () => {
-    const modifiedContext = {...testContext, favourites: [ampharosPokeData, jumpluffPokeData]}
-      const {favourites} = removeItem(modifiedContext, ampharosPokeData)
+      const modifiedContext = {
+        ...testContext,
+        favourites: [ampharosPokeData, jumpluffPokeData],
+      }
+      const { favourites } = removeItem(modifiedContext, ampharosPokeData)
       expect(favourites?.length).toEqual(1)
     })
   })
 
   describe('setSelectedItem()', () => {
     test('should set data to select', () => {
-      const {selected} = setSelectedItem(testContext, ampharosPokeData)
+      const { selected } = setSelectedItem(testContext, ampharosPokeData)
       expect(selected).toBeDefined()
     })
   })
 
   describe('isInFavourites()', () => {
     test('should return true if data is in favourites', () => {
-      expect(isInFavourites( [ampharosPokeData, jumpluffPokeData], 189)).toBeTruthy()
-      expect(isInFavourites( [ampharosPokeData, jumpluffPokeData], 2)).toBeFalsy()
+      expect(
+        isInFavourites([ampharosPokeData, jumpluffPokeData], 189)
+      ).toBeTruthy()
+      expect(
+        isInFavourites([ampharosPokeData, jumpluffPokeData], 2)
+      ).toBeFalsy()
     })
   })
 })
