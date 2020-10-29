8943729a7b29e7d9dc77484de19c81248269be47
Marco Sangalli <sangalli.marco@gmail.com>
2020-10-29 12:45:48 +0100

    formatting code

--- src/store/favourites/__tests__/actions.spec.ts
+++ src/store/favourites/__tests__/actions.spec.ts
@@ -7,25 +7,22 @@ function returnPokeData(data: any): PokeData {
   return data as PokeData
 }
 
-
 describe('Actions', () => {
-    const testData = returnPokeData(ampharos)
+  const testData = returnPokeData(ampharos)
 
-    it('should create an action to add', () => {
-        const expectedAction = {
-            type: ADD,
-            payload: testData
-        }
-        expect(addItemAction(testData)).toEqual(expectedAction)
-    })
+  it('should create an action to add', () => {
+    const expectedAction = {
+      type: ADD,
+      payload: testData,
+    }
+    expect(addItemAction(testData)).toEqual(expectedAction)
+  })
 
-
-    it('should create an action to remove', () => {
-        const expectedAction = {
-            type: REMOVE,
-            payload: testData
-        }
-        expect(removeItemAction(testData)).toEqual(expectedAction)
-    })
-
+  it('should create an action to remove', () => {
+    const expectedAction = {
+      type: REMOVE,
+      payload: testData,
+    }
+    expect(removeItemAction(testData)).toEqual(expectedAction)
+  })
 })
--- src/store/list/__tests__/reducers.spec.ts
+++ src/store/list/__tests__/reducers.spec.ts
@@ -1,41 +1,39 @@
-import { ListReducer } from "../reducers"
-import { FILTER, SET_LIST } from "../types"
+import { ListReducer } from '../reducers'
+import { FILTER, SET_LIST } from '../types'
 import ampharos from '../../__tests__/ampharos.json'
-import { PokeData } from "../../../shared/types"
+import { PokeData } from '../../../shared/types'
 
 function returnPokeData(data: any): PokeData {
-    return data as PokeData
+  return data as PokeData
 }
 
 describe('ListReducer', () => {
-    const testList = [returnPokeData(ampharos)]
+  const testList = [returnPokeData(ampharos)]
 
-    it('should return the initial state', () => {
-        expect(ListReducer(undefined, {})).toEqual(
-            { "filter": "", "limit": 50, "list": [], "offset": 0 }
-        )
+  it('should return the initial state', () => {
+    expect(ListReducer(undefined, {})).toEqual({
+      filter: '',
+      limit: 50,
+      list: [],
+      offset: 0,
     })
+  })
 
-    it('should handle set filter', () => {
-        expect(
-            ListReducer(undefined, {
-                type: FILTER,
-                payload: 'Amp'
-            })
-        ).toEqual(
-            { "filter": "Amp", "limit": 50, "list": [], "offset": 0 }
-        )
-    })
+  it('should handle set filter', () => {
+    expect(
+      ListReducer(undefined, {
+        type: FILTER,
+        payload: 'Amp',
+      })
+    ).toEqual({ filter: 'Amp', limit: 50, list: [], offset: 0 })
+  })
 
-
-    it('should handle set list', () => {
-        expect(
-            ListReducer(undefined, {
-                type: SET_LIST,
-                payload: testList
-            })
-        ).toEqual(
-            { "filter": "", "limit": 50, "list": testList, "offset": 0 }
-        )
-    })
+  it('should handle set list', () => {
+    expect(
+      ListReducer(undefined, {
+        type: SET_LIST,
+        payload: testList,
+      })
+    ).toEqual({ filter: '', limit: 50, list: testList, offset: 0 })
+  })
 })
